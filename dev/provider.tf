384740a4aa790d764e7fbc54720a8d4b74e056cf
Subramanyam Vemala <64848680+subbugh@users.noreply.github.com>
2024-05-23 18:22:14 +0530

    Update provider.tf

--- dev/provider.tf
+++ dev/provider.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0.0"
-      #version = "~> 5.50.0"
+      source = "hashicorp/aws"
+      #version = "~> 4.0.0"
+      version = "~> 5.50.0"
     }
   }
 }
